b3bd73ccaa0e4c2609ccb3a63636af65c2fcaff8
anishere <tranbinhan1100@gmail.com>
2023-08-12 11:27:05 +0700

    update source code

---
+++ apiProducts/Controllers/ProductsController.cs
@@ -0,0 +1,221 @@
+﻿using apiProducts.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace apiProducts.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public ProductsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("ProductList")]
+
+        public Response GetAllProducts ()
+        {
+            List<Products> lstproducts = new List<Products>();
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Product").ToString());
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Products", connection);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            Response response = new Response();
+            if(dt.Rows.Count > 0) {
+                for(int i = 0; i < dt.Rows.Count; i++)
+                {
+                    Products products = new Products();
+                    products.ProductID = Convert.ToInt32(dt.Rows[i]["ProductID"]);
+                    products.ProductName = Convert.ToString(dt.Rows[i]["ProductName"]);
+                    products.Discription = Convert.ToString(dt.Rows[i]["Discription"]);
+                    products.Detail = Convert.ToString(dt.Rows[i]["Detail"]);
+                    products.Brand = Convert.ToString(dt.Rows[i]["Brand"]);
+                    products.Discount = Convert.ToDecimal(dt.Rows[i]["Discount"]);
+                    products.Price = Convert.ToDecimal(dt.Rows[i]["Price"]);
+                    products.Image = Convert.ToString(dt.Rows[i]["Image"]);
+                    products.Type = Convert.ToString(dt.Rows[i]["Type"]);
+                    lstproducts.Add(products);
+                }
+                if(lstproducts.Count > 0)
+                {
+                    response.StatusCode = 200;
+                    response.StatusMessage = "Data found";
+                    response.listproducts = lstproducts;
+                }
+                else
+                {
+                    response.StatusCode = 100;
+                    response.StatusMessage = "No data found";
+                    response.listproducts = null;
+                }
+            }
+            else
+            {
+                response.StatusCode = 100;
+                response.StatusMessage = "No data found";
+                response.listproducts = null;
+            }
+            return response;
+        }
+
+        [HttpPost]
+        [Route("AddProduct")]
+
+        public Response AddProduct(Products obj)
+        {
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Product").ToString());
+            Response response = new Response();
+
+            try
+            {
+                connection.Open();
+
+                string query = "INSERT INTO Products (ProductName, Discription, Detail, Brand, Discount, Price, Image, Type) " +
+                               "VALUES (@ProductName, @Discription, @Detail, @Brand, @Discount, @Price, @Image, @Type)";
+
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@ProductName", obj.ProductName);
+                    cmd.Parameters.AddWithValue("@Discription", obj.Discription);
+                    cmd.Parameters.AddWithValue("@Detail", obj.Detail);
+                    cmd.Parameters.AddWithValue("@Brand", obj.Brand);
+                    cmd.Parameters.AddWithValue("@Discount", obj.Discount);
+                    cmd.Parameters.AddWithValue("@Price", obj.Price);
+                    cmd.Parameters.AddWithValue("@Image", obj.Image);
+                    cmd.Parameters.AddWithValue("@Type", obj.Type);
+
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        response.StatusCode = 200;
+                        response.StatusMessage = "Product added successfully";
+                    }
+                    else
+                    {
+                        response.StatusCode = 100;
+                        response.StatusMessage = "Failed to add product";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                response.StatusCode = 500;
+                response.StatusMessage = "An error occurred: " + ex.Message;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return response;
+        }
+
+        [HttpDelete]
+        [Route("DeleteProduct/{id}")]
+        public Response DeleteProduct(int id)
+        {
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Product").ToString());
+            Response response = new Response();
+
+            try
+            {
+                connection.Open();
+
+                string query = "DELETE FROM Products WHERE ProductID = @ProductID";
+
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@ProductID", id);
+
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        response.StatusCode = 200;
+                        response.StatusMessage = "Product deleted successfully";
+                    }
+                    else
+                    {
+                        response.StatusCode = 100;
+                        response.StatusMessage = "Product not found or failed to delete";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                response.StatusCode = 500;
+                response.StatusMessage = "An error occurred: " + ex.Message;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return response;
+        }
+
+        [HttpPut]
+        [Route("UpdateProduct/{id}")]
+        public Response UpdateProduct(int id, Products updatedProduct)
+        {
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Product").ToString());
+            Response response = new Response();
+
+            try
+            {
+                connection.Open();
+
+                string query = "UPDATE Products " +
+                               "SET ProductName = @ProductName, Discription = @Discription, " +
+                               "Detail = @Detail, Brand = @Brand, Discount = @Discount, " +
+                               "Price = @Price, Image = @Image, Type = @Type " +
+                               "WHERE ProductID = @ProductID";
+
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@ProductID", id);
+                    cmd.Parameters.AddWithValue("@ProductName", updatedProduct.ProductName);
+                    cmd.Parameters.AddWithValue("@Discription", updatedProduct.Discription);
+                    cmd.Parameters.AddWithValue("@Detail", updatedProduct.Detail);
+                    cmd.Parameters.AddWithValue("@Brand", updatedProduct.Brand);
+                    cmd.Parameters.AddWithValue("@Discount", updatedProduct.Discount);
+                    cmd.Parameters.AddWithValue("@Price", updatedProduct.Price);
+                    cmd.Parameters.AddWithValue("@Image", updatedProduct.Image);
+                    cmd.Parameters.AddWithValue("@Type", updatedProduct.Type);
+
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        response.StatusCode = 200;
+                        response.StatusMessage = "Product updated successfully";
+                    }
+                    else
+                    {
+                        response.StatusCode = 100;
+                        response.StatusMessage = "Product not found or failed to update";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                response.StatusCode = 500;
+                response.StatusMessage = "An error occurred: " + ex.Message;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return response;
+        }
+    }
+}
---
+++ apiProducts/Models/Products.cs
@@ -0,0 +1,23 @@
+﻿namespace apiProducts.Models
+{
+    public class Products
+    {
+        public int ProductID { get; set; }
+
+        public string? ProductName { get; set; }
+
+        public string? Discription { get; set; }
+
+        public string? Detail { get; set; }
+
+        public string? Brand { get; set; }
+
+        public decimal Discount { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string? Image { get; set; }
+
+        public string? Type { get; set; }
+    }
+}
